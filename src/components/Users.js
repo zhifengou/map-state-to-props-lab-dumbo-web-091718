d24eddba766d28faaa1da07dbf40da3056186f90
zhifeng ou <zhifengou@hotmail.com>
2018-12-07 23:25:16 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
 class Users extends Component {
-
-  render() {
+  let users = this.props.users.map((u,index)=>{ return <li key={index}>{u.username}</li>})
     return (
       <div>
         <ul>
-          Users!
+          {users}
         </ul>
+        <p>Total: {users.length}</p>
       </div>
     )
   }
 }
 
 //add mapStateToProps here
-
+const mapStateToProps = (state)=>{
+  return {users: state.users}
+}
 export default Users
